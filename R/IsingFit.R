b9f7ebfb77f60c6e8bebfa13391193106a5b523d
Claudia van Borkulo <cvborkulo@gmail.com>
2014-01-07 00:00:00 +0000

    version 0.1.2

---
+++ R/IsingFit.R
@@ -0,0 +1,124 @@
+IsingFit <-
+function(x, family='binomial', AND = TRUE, gamma = 0.25, plot = TRUE, progressbar = TRUE, ...){
+  t0 <- Sys.time()
+  if (family!='binomial'&family!='gaussian') 
+    stop ("This procedure is currently only supported for binary (family='binomial') or continuous data (family='gaussian')")
+  NodesToAnalyze <- apply(x,2,sd, na.rm=TRUE) != 0
+  names(NodesToAnalyze) <- colnames(x)
+  if (!any(NodesToAnalyze)) stop("No variance in dataset")
+  if (any(!NodesToAnalyze))
+  {
+    warning(paste("Nodes without variance:",paste(colnames(x)[!NodesToAnalyze],collapse = ", ")))
+  }
+  x <- as.matrix(x)
+  allthemeans <- colMeans(x)
+  x <- x[,NodesToAnalyze,drop=FALSE]
+  nvar <- ncol(x)
+  p <- nvar - 1
+  intercepts <- betas <- lambdas <- list(vector,nvar)
+  nlambdas <- rep(0,nvar)
+  for (i in 1: nvar){
+    a <- glmnet(x[,-i], x[,i], family = family)
+    intercepts[[i]] <- a$a0
+    betas[[i]] <- a$beta
+    lambdas[[i]] <- a$lambda
+    nlambdas[i] <- length(lambdas[[i]])
+  }
+  if (progressbar==TRUE) pb <- txtProgressBar(max=nrow(x), style = 3)
+  P <- logl <- sumlogl <- J <- matrix(0, max(nlambdas), nvar)
+  for (i in 1:nvar)
+  {
+    J[1:ncol(betas[[i]]),i] <- colSums(betas[[i]]!=0)
+  }
+  for (n in 1:nrow(x)){
+    for (i in 1: nvar){
+      y <- intercepts[[i]] + colSums(betas[[i]]*x[n,-i])
+      y <- c(y,rep(NA,max(nlambdas)-length(y)))
+      P[,i] <- exp(y*x[n,i])/(1+exp(y))
+      logl[,i] <- log(P[,i])
+    }
+    sumlogl <- sumlogl + logl
+    if (progressbar==TRUE) setTxtProgressBar(pb, n)
+  }
+  if (progressbar==TRUE) close(pb)
+  sumlogl[sumlogl==0]=NA
+  penalty <- J * log(nrow(x)) + 2 * gamma * J * log(p)
+  EBIC <- -2 * sumlogl + penalty
+  lambda.opt <- apply(EBIC,2,which.min)
+  thresholds <- 0
+  for(i in 1:length(lambda.opt))
+    thresholds[i] <- intercepts[[i]][lambda.opt[i]]
+  weights.opt <- matrix(,nvar,nvar)
+  for (i in 1:nvar){
+    weights.opt[i,-i] <- betas[[i]][,lambda.opt[i]]
+  }
+  if (AND==TRUE) {
+    adj <- weights.opt
+#     adj[weights.opt!=0] <- 1
+#     adj[weights.opt=0] <- 0
+    adj <- (adj!=0)*1
+    EN.weights <- adj * t(adj)
+    EN.weights <- EN.weights * weights.opt
+    meanweights.opt <- (EN.weights+t(EN.weights))/2
+    diag(meanweights.opt) <- 0 
+  } else {
+    meanweights.opt <- (weights.opt+t(weights.opt))/2
+    diag(meanweights.opt) <- 0
+  }
+  graphNew <- matrix(0,length(NodesToAnalyze),length(NodesToAnalyze))
+  graphNew[NodesToAnalyze,NodesToAnalyze] <- meanweights.opt
+  colnames(graphNew) <- rownames(graphNew) <- colnames(x)
+  threshNew <- ifelse(allthemeans > 0.5, -Inf, Inf)
+  threshNew[NodesToAnalyze] <- thresholds
+  if (plot==TRUE) notplot=FALSE else notplot=TRUE
+  q <- qgraph(graphNew,layout='spring',labels=names(NodesToAnalyze),DoNotPlot=notplot,...)
+  Res <- list(weiadj = graphNew, thresholds = threshNew, q = q, gamma = gamma, AND = AND, time = Sys.time() - t0)
+  class(Res) <- "IsingFit"
+  return(Res)
+}
+
+## Methods:
+plot.IsingFit <- function(object,...) qgraph(object$q,DoNotPlot = FALSE, ...)
+
+print.IsingFit <- function(x)
+{
+  cat("Estimated network:\n")
+  
+  print(round(x$weiadj,2))
+  
+  cat("\n\nEstimated Thresholds:\n")
+  
+  print(x$thresholds)  
+}
+
+summary.IsingFit <- function(object)
+{
+  cat("\tNetwork Density:\t\t", round(mean(object$weiadj[upper.tri(object$weiadj)]!=0),2),"\n",
+      "Gamma:\t\t\t",round(object$gamma,2),"\n",
+      "Rule used:\t\t",ifelse(object$AND,"And-rule","Or-rule"),"\n",
+      "Analysis took:\t\t",format(object$time,format="%s"),"\n"
+  )
+}
+
+exportNetLogo.IsingFit <- function(x)
+{
+  if (is.character(x))
+  {
+    x[] <- paste0('"',x,'"')
+  }
+  if (is.vector(x))
+  {
+    return(paste("\n",paste(paste0("[",paste(x, collapse = " "),"]"),collapse="\n"),"\n\n"))
+  } else if (is.matrix(x))
+  {
+    return(paste("[\n",paste(apply(x,1,function(s)paste0("[",paste(s, collapse = " "),"]")),collapse="\n"),"\n]")  )
+  } else stop("Object not supported")
+  
+}
+
+# 
+# print(fit)
+# plot(fit)
+# summary(fit)
+# export(fit)
+
---
+++ man/isingfit.rd
@@ -0,0 +1,88 @@
+\name{IsingFit}
+\alias{IsingFit}
+\title{
+Network estimation using the eLasso method
+}
+\description{
+This network estimation procedure eLasso, which is based on the Ising model, combines l1-regularized logistic regression with model selection based on the Extended Bayesian Information Criterion (EBIC). EBIC is a fit measure that identifies relevant relationships between variables. The resulting network consists of variables as nodes and relevant relationships as edges. Can deal with binary or continuous data.
+}
+\usage{
+IsingFit(x, family='binomial', AND = TRUE, gamma = 0.25, 
+plot = TRUE, progressbar = TRUE, ...)
+}
+
+\arguments{
+  \item{x}{
+Input matrix. The dimension of the matrix is nobs x nvars; each row is a vector of observations of the variables. Must be cross-sectional data.
+}
+\item{family}{
+If 'gaussian' then the data is treated as being continuous. The default is 'binomial', treating the data as binary. Currently, this procedure is only supported for binary or continuous data.
+}
+  \item{AND}{
+Logical. Can be TRUE of FALSE to indicate whether the AND-rule or the OR-rule should be used to define the edges in the network. Defaults to TRUE.
+}
+  \item{gamma}{
+A value of hyperparameter gamma in the extended BIC. Can be anything between 0 and 1. Defaults to .25.
+}
+  \item{plot}{
+Logical. Should the resulting network be plotted?
+}
+  \item{progressbar}{
+Logical. Should the pbar be plotted in order to see the progress of the estimation procedure?
+}
+\item{\dots}{
+Arguments sent to \code{qgraph}.
+}
+}
+
+\value{
+IsingFit returns (invisibly) a 'IsingFit' object that contains the following items:
+\item{weiadj }{The weighted adjacency matrix.}
+\item{thresholds }{Thresholds of the variables.}
+\item{q }{The object that is returned by qgraph (class 'qgraph').}
+\item{gamma }{The value of hyperparameter gamma.}
+\item{AND }{A logical indicating whether the AND-rule is used or not. If not, the OR-rule is used.}
+\item{time }{The time it took to estimate the network.}
+}
+\references{
+Van Borkulo, C.D., Borsboom, D., Epskamp, S., Blanken, T., Bosschloo, L., Schoevers, R. A., & Waldorp, L. J. (2013). Fitting like a glove: A new method for constructing networks for psychometric data. Manuscript submitted for publication.
+}
+\author{
+Claudia van Borkulo <cvborkulo@gmail.com>
+}
+\note{
+See also my website: http://cvborkulo.com
+}
+
+\examples{
+library("IsingSampler")
+
+### Simulate dataset ###
+# Input:
+N <- 6 # Number of nodes
+nSample <- 1000 # Number of samples
+
+# Ising parameters:
+Graph <- matrix(sample(0:1,N^2,TRUE,prob = c(0.8, 0.2)),N,N) * runif(N^2,0.5,2)
+Graph <- pmax(Graph,t(Graph))
+diag(Graph) <- 0
+Thresh <- -rowSums(Graph) / 2
+
+# Siumlate:
+Data <- IsingSampler(nSample, Graph, Thresh)
+
+### Fit using IsingFit ###
+Res <- IsingFit(Data, family='binomial', plot=FALSE)
+
+# Plot results:
+library("qgraph")
+layout(t(1:2))
+qgraph(Res$weiadj,fade = FALSE)
+title("Estimated network")
+qgraph(Graph,fade = FALSE)
+title("Original network")
+}
+% Add one or more standard keywords, see file 'KEYWORDS' in the
+% R documentation directory.
+% \keyword{ ~kwd1 }
+% \keyword{ ~kwd2 }% __ONLY ONE__ keyword per line
